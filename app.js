da7b1784be285cd3da054673b81ac9f98a83d1bf
julieta bariandaran <julietabdr@icloud.com>
2023-06-26 16:18:34 -0300

    fav
    intento de fav

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const cargarPeliculas = async () => {
   try {
     const respuesta = await fetch(
@@ -13,15 +14,16 @@ const cargarPeliculas = async () => {
                 <div class="card" style="width: 15rem;">
                 <img src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" class="card-img-top" alt="${pelicula.title}">
                 <div class="card-body">
-                <i class="bi bi-heart iconoFav"></i>
+                <button class="like_btn">
+                <span id="icon"><i class="bi bi-heart"></i></span>
+                </button>
                 <h5 class="card-title">${pelicula.title}</h5>
                 <a href="detalle.html?id=${pelicula.id}" class="btn btn-peliculas">Ver detalles</a>
                 </div>
                 </div>
-           
-                
                 `;
       });
+
       document.getElementById("contenedor").innerHTML = peliculas;
     } else if (respuesta.status === 401) {
       console.log("Pusiste la llave mal");
@@ -33,6 +35,7 @@ const cargarPeliculas = async () => {
   } catch (error) {
     console.log(error);
   }
+
   const btnBuscar = document.getElementById('buscar');
   const peliculasBuscadas = document.getElementById('peliculasBuscadas');
   btnBuscar.addEventListener('click', buscarPeliculas);
@@ -40,6 +43,24 @@ const cargarPeliculas = async () => {
     e.preventDefault();
     const searchInput = document.getElementById('search-input').value;
   
+    const likeBtn = document.querySelector(".like_btn");
+    let likeIcon = document.querySelector("#icon");
+    let clicked = false;
+    
+    
+     likeBtn.addEventListener("click", () => {
+            if (clicked){
+              clicked = true;
+              likeIcon.innerHTML = `<i class="bi bi-heart-fill"></i>`;
+              
+            }else{
+              clicked = false;
+              likeIcon.innerHTML = `<i class="bi bi-heart"></i>`;
+              
+            }
+          });
+        
+
     try {
       const respuesta = await fetch(
         `https://api.themoviedb.org/3/search/movie?api_key=47e33bf29ef2fc5eaefdc1cd1ed4b0df&language=es-MX&query=${searchInput}`
@@ -56,6 +77,7 @@ const cargarPeliculas = async () => {
       console.error('Error al buscar películas:', error);
     }
   }
+
  
   function verPeliculas(pelicula) {
     
@@ -91,3 +113,5 @@ const cargarPeliculas = async () => {
 cargarPeliculas();
 
 
+
+   
