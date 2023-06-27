50f4fe2d85cdc1b9148ceb5e4d318cd9242c4773
ailenad <ailen.alvarez@davinci.edu.ar>
2023-06-27 11:01:14 -0300

    funcionalidad favoritos

---
+++ scripts/app.js
@@ -0,0 +1,131 @@
+
+const cargarPeliculas = async () => {
+  try {
+    const respuesta = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=47e33bf29ef2fc5eaefdc1cd1ed4b0df&language=es-MX"
+    );
+    console.log(respuesta);
+
+    if (respuesta.status === 200) {
+      const datos = await respuesta.json();
+      let peliculas = "";
+      datos.results.forEach((pelicula) => {
+        peliculas += `
+                <div class="card" style="width: 15rem;">
+                <img src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" class="card-img-top" alt="${pelicula.title}">
+                <div class="card-body">
+                <button class="like_btn" data-pelicula-id="${pelicula.id}">
+                <span id="icon"><i class="bi bi-heart"></i></span>
+                </button>
+                <h5 class="card-title">${pelicula.title}</h5>
+                <a href="detalle.html?id=${pelicula.id}" class="btn btn-peliculas">Ver detalles</a>
+                </div>
+                </div>
+                `;
+      });
+
+      document.getElementById("contenedor").innerHTML = peliculas;
+    } else if (respuesta.status === 401) {
+      console.log("Pusiste la llave mal");
+    } else if (respuesta.status === 404) {
+      console.log("La pelicula que buscas no existe");
+    } else {
+      console.log("Error no identificado");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  const btnBuscar = document.getElementById('buscar');
+  const peliculasBuscadas = document.getElementById('peliculasBuscadas');
+  btnBuscar.addEventListener('click', buscarPeliculas);
+  async function buscarPeliculas(e) {
+    e.preventDefault();
+    const searchInput = document.getElementById('search-input').value;
+
+    try {
+      const respuesta = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=47e33bf29ef2fc5eaefdc1cd1ed4b0df&language=es-MX&query=${searchInput}`
+      );
+  
+      if (respuesta.status === 200) {
+        const datos = await respuesta.json();
+        document.getElementById("contenedor").style.display = "none";
+        verPeliculas(datos.results);
+      } else {
+        console.log('Error al buscar películas');
+      }
+    } catch (error) {
+      console.error('Error al buscar películas:', error);
+    }
+  }
+  function verPeliculas(pelicula) {
+    
+   peliculasBuscadas.innerHTML = '';
+  
+    if (pelicula.length === 0) {
+      peliculasBuscadas.textContent = 'No se encontraron películas.';
+    } else {
+      pelicula.forEach((pelicula) => {
+        const pElement = document.createElement('div');
+        pElement.innerHTML = `
+        <div class="card" style="width: 15rem;">
+        <img src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" class="card-img-top" alt="${pelicula.title}">
+        <div class="card-body">
+        <button class="like_btn">
+                <span id="icon"><i class="bi bi-heart"></i></span>
+        </button>
+        <h5 class="card-title">${pelicula.title}</h5>
+        <a href="detalle.html?id=${pelicula.id}" class="btn btn-peliculas">Ver detalles</a>
+        </div>
+        </div>
+        `;
+        peliculasBuscadas.appendChild(pElement);
+      });
+      const volverBtn = document.createElement('button');
+      volverBtn.textContent = 'Volver';
+      volverBtn.className = 'btn btn-volver';
+      volverBtn.addEventListener('click', () => {
+      peliculasBuscadas.style.display = "none";
+      //  document.getElementById("detalle");
+      });
+      peliculasBuscadas.append(volverBtn);
+      favoritos();
+    }
+  }
+  function favoritos() {
+    const likeBtns = document.querySelectorAll(".like_btn");
+    const pelisFav = JSON.parse(localStorage.getItem("favoritos")) || [];
+    likeBtns.forEach((likeBtn) => {
+      const likeIcon = likeBtn.querySelector("span#icon");
+      const peliculaId = likeBtn.dataset.peliculaId;
+      let clicked = pelisFav.includes(peliculaId);
+
+      if (clicked) {
+        likeIcon.innerHTML = `<i class="bi bi-heart-fill text-danger"></i>`;
+      }
+
+      likeBtn.addEventListener("click", () => {
+        clicked = !clicked;
+        if (clicked) {
+          likeIcon.innerHTML = `<i class="bi bi-heart-fill text-danger"></i>`;
+          pelisFav.push(peliculaId);
+          console.log(pelisFav);
+        } else {
+          likeIcon.innerHTML = `<i class="bi bi-heart"></i>`;
+          const index = pelisFav.indexOf(peliculaId);
+        if (index > -1) {
+          pelisFav.splice(index, 1);
+           console.log(pelisFav);
+        }
+        }
+        localStorage.setItem("favoritos", JSON.stringify(pelisFav));
+      });
+    });
+  }
+  favoritos();
+
+};
+cargarPeliculas();
+
+   
--- scripts/favoritos.js
+++ scripts/favoritos.js
@@ -1,80 +1,81 @@
 
-// const favoritosContainer = document.getElementById('favoritos-contenedor');
- // Obtén la lista de favoritos (puedes implementar la función obtenerListaFavoritos())
+const pelisFav = JSON.parse(localStorage.getItem("favoritos"));
+const divFavoritos = document.getElementById('peliculasFavoritas');
 
-// Recorre la lista de favoritos y muestra los detalles de cada película
-listaFavoritos.forEach((peliculaId) => {
-  obtenerDetallesPelicula(peliculaId)
-    .then((pelicula) => {
-      mostrarPelicula(pelicula);
-    })
-    .catch((error) => {
-      console.error('Error al obtener los detalles de la película:', error);
-    });
+const eliminarFavoritos = (peliculaId) => {
+  const index = pelisFav.indexOf(peliculaId);
+  if (index > -1) {
+    pelisFav.splice(index, 1);
+    console.log(pelisFav);
+    // Actualiza el almacenamiento local con la lista actualizada de favoritos
+    localStorage.setItem("favoritos", JSON.stringify(pelisFav));
+
+    // Elimina la tarjeta correspondiente
+    const card = document.getElementById(`card-${peliculaId}`);
+    if (card) {
+      card.remove();
+    }
+  }
+};
+
+const verFavoritos = async () => {
+  try {
+    divFavoritos.innerHTML = ""; 
+
+    for (const peliculaId of pelisFav ) {
+      const respuesta = await fetch(
+        `https://api.themoviedb.org/3/movie/${peliculaId}?api_key=47e33bf29ef2fc5eaefdc1cd1ed4b0df&language=es-MX`
+      );
+
+      if (respuesta.status === 200) {
+        const datos = await respuesta.json();
+        const titulo = datos.title;
+        const descripcion = datos.overview;
+        const anoLanzamiento = datos.release_date;
+        const poster = datos.poster_path;
+        const card = document.createElement("div");
+        card.id = `card-${peliculaId}`;
+        card.classList.add("card", "mb-3", "detalleCard");
+        card.style.maxWidth = "540px";
+        card.innerHTML = `
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="https://image.tmdb.org/t/p/w500/${poster}" class="img-fluid rounded-start" alt="${titulo}">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${titulo}</h5>
+                <p class="card-text">${descripcion}</p>
+                <p class="descripcion">Año de lanzamiento: ${anoLanzamiento}</p>
+                <button class="btn eliminarFavBtn" data-pelicula-id="${peliculaId}">Eliminar de favoritos</button>
+              </div>
+            </div>
+          </div>
+        `;
+
+        divFavoritos.appendChild(card);
+      } else if (respuesta.status === 401) {
+        console.log("Pusiste la llave mal");
+      } else if (respuesta.status === 404) {
+        console.log("La película que buscas no existe");
+      } else {
+        console.log("Error no identificado");
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+verFavoritos();
+
+divFavoritos.addEventListener('click', (event) => {
+  if (event.target.classList.contains('eliminarFavBtn')) {
+    const peliculaId = event.target.dataset.peliculaId;
+    eliminarFavoritos(peliculaId);
+  }
 });
 
-// Función para obtener los detalles de una película según su ID
-async function obtenerDetallesPelicula(peliculaId) {
-  const url = `https://api.themoviedb.org/3/movie/${peliculaId}?api_key=47e33bf29ef2fc5eaefdc1cd1ed4b0df&language=es-MX`;
-  const respuesta = await fetch(url);
-  const datos = await respuesta.json();
-  return datos;
-}
-
-// Función para mostrar los detalles de una película en el contenedor
-// function mostrarPelicula(pelicula) {
-//   const peliculaElement = document.createElement('div');
-//   peliculaElement.innerHTML = `
-//     <div class="card">
-//       <img src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" alt="${pelicula.title}">
-//       <div class="card-body">
-//         <h5 class="card-title">${pelicula.title}</h5>
-//         <p class="card-text">${pelicula.overview}</p>
-//       </div>
-//     </div>
-//   `;
-//   favoritosContainer.appendChild(peliculaElement);
-// }
-// mostrarPelicula()
-const div = document.getElementById('favoritos-contenedor')
-const mostrarPelicula= async () =>{
-    try {
-        const respuesta = await fetch(
-            `https://api.themoviedb.org/3/movie/${peliculaId}?api_key=47e33bf29ef2fc5eaefdc1cd1ed4b0df&language=es-MX`
-        );
-        console.log(respuesta);
-    
-        if (respuesta.status === 200) {
-          const datos = await respuesta.json();
-            const titulo = datos.title;
-            const descripcion = datos.overview;
-            const anoLanzamiento = datos.release_date;
-            const poster = datos.poster_path;
-            div.innerHTML="";
-            div.innerHTML=`
-                <div class="card mb-3 detalleCard" style="max-width: 540px;">
-                <div class="row g-0">
-                  <div class="col-md-4">
-                  <img src="https://image.tmdb.org/t/p/w500/${poster}"class="img-fluid rounded-start" alt="${titulo}">
-                  </div>
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title">${titulo}</h5>
-                      <p class="card-text">${descripcion}</p>
-                     <p class="descripcion">Año de lanzamiento: ${anoLanzamiento}</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-                `;
-        } else if (respuesta.status === 401) {
-          console.log("Pusiste la llave mal");
-        } else if (respuesta.status === 404) {
-          console.log("La pelicula que buscas no existe");
-        } else {
-          console.log("Error no identificado");
-        }
-      } catch (error) {
-        console.log(error);
-      } 
-}
+volver.addEventListener('click', () => {
+  window.location.href = 'index.html';
+});
